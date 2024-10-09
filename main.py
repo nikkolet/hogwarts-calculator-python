83211ab8acbf5cf6a553c75e4d33a705f2ecce70
nikkolet <nikkoletashby@Gmail.com>
2024-10-09 07:28:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    pass
+
+def evaluate_calculation():
+    pass
+
+def clear_field():
+    pass
+
+
+root = tk.Tk() # Start GUI
+root.geometry("500x500")
+root.mainloop() #End GUI
 
