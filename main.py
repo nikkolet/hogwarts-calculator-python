16a3648767372c7a60724ef0edc39e8530e26396
nikkolet <nikkoletashby@Gmail.com>
2024-10-09 09:28:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,39 @@ import tkinter as tk
 calculation = ""
 
 def add_to_calculation(symbol):
-    pass
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0, "end")
+    text_result.insert(1.0, calculation)
 
 def evaluate_calculation():
-    pass
+    global calculation
+    try: 
+        calculation = str(eval(calculation))
+        calculation = ""
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, calculation)
+    except: 
+        clear_field()
+        text_result.insert(1.0, "Error")
 
 def clear_field():
-    pass
-
+    global calculation
+    calculation = ""
+    text_result.delete(1.0, "end")
 
 root = tk.Tk() # Start GUI
 root.geometry("500x500")
+
+
+text_result = tk.Text(root, height=2, width=16, font=("Ariel",24))
+text_result.grid(columnspan=5)
+root.mainloop()
+
+
+
+
+
+
 root.mainloop() #End GUI
 
